d6e8690ff08ca14ad9ad62f6d654ab3240632187
pestophagous <pestophagous@gmail.com>
2020-06-15 22:09:22 -0700

    enable CI for Mac. build, launch, and test automatically on macos.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -37,8 +37,14 @@ else
   XDISPLAY=""
 fi
 
-tools/formatters/enforce_clang_format.sh check_only
-tools/formatters/enforce_qml_format.sh check_only
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  # TODO: we can make it work on mac with clang-format-10
+  tools/formatters/enforce_clang_format.sh check_only
+
+  # TODO: we already made this work in a different repo. learn from it.
+  tools/formatters/enforce_qml_format.sh check_only
+fi
+
 
 ./build_app.sh
 
@@ -46,10 +52,12 @@ tools/formatters/enforce_qml_format.sh check_only
 tools/auto_test/run_cpp_auto_tests.sh
 
 if [[ -n ${XDISPLAY-} ]]; then
-  # 'coproc' so that SIGINT will still work (https://unix.stackexchange.com/a/478697/11592)
-  coproc Xvfb ${XDISPLAY} -screen 0 1024x768x16
-  VIRT_FB_PID=$!
-  sleep 4 # time to (probabilistically) ensure that Xvfb has started
+  if [[ "$OSTYPE" != "darwin"* ]]; then
+    # 'coproc' so that SIGINT will still work (https://unix.stackexchange.com/a/478697/11592)
+    coproc Xvfb ${XDISPLAY} -screen 0 1024x768x16
+    VIRT_FB_PID=$!
+    sleep 4 # time to (probabilistically) ensure that Xvfb has started
+  fi
 fi
 
 # run gui tests which execute the actual app binary:
@@ -61,5 +69,7 @@ if [[ -z ${MYAPP_TEMPLATE_SKIP_APPIMAGE-} ]]; then
 fi
 
 if [[ -n ${XDISPLAY-} ]]; then
-   kill -SIGINT $VIRT_FB_PID
+  if [[ "$OSTYPE" != "darwin"* ]]; then
+    kill -SIGINT $VIRT_FB_PID
+  fi
 fi
--- tools/gui_test/launch_gui_for_display.sh
+++ tools/gui_test/launch_gui_for_display.sh
@@ -10,16 +10,22 @@
 
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
-echo "reading single argument (should be either empty or ':1' to choose display)"
-export DISPLAY="${1:-"$DISPLAY"}"
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  echo "reading single argument (should be either empty or ':1' to choose display)"
+  export DISPLAY="${1:-"$DISPLAY"}"
+fi
 
 CUR_GIT_ROOT=$(git rev-parse --show-toplevel)
 
 cd $CUR_GIT_ROOT
 rm -f gui_test.log # in C.I. there should never be a leftover file. but perhaps locally.
 
-# -g flag causes app to close when test is done:
-gdb -batch -return-child-result -ex "set args -g -v" -ex "run" -ex "bt" ${CUR_GIT_ROOT}/build/src/app/app 2>&1 |& tee gui_test.log
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  # -g flag causes app to close when test is done:
+  gdb -batch -return-child-result -ex "set args -g -v" -ex "run" -ex "bt" ${CUR_GIT_ROOT}/build/src/app/app 2>&1 | tee gui_test.log
+else
+  build/src/app/app.app/Contents/MacOS/app -g 2>&1 | tee gui_test.log
+fi
 
 tools/gui_test/check_gui_test_log.py gui_test.log
 rm -f gui_test.log
