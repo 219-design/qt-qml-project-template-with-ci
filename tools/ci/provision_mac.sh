c586d000ae96f0bc352382d0a52a97676493ef61
pestophagous <pestophagous@users.noreply.github.com>
2020-06-17 08:10:21 -0700

    provision_mac.sh: make 'ln' usage idempotent

--- tools/ci/provision_mac.sh
+++ tools/ci/provision_mac.sh
@@ -21,7 +21,7 @@ brew install \
      xz
 
 pushd /usr/local/bin >& /dev/null
-  sudo ln -s clang-format clang-format-10
+  sudo ln -sf clang-format clang-format-10
 popd >& /dev/null
 
 ${CUR_GIT_ROOT}/tools/ci/get_qt_libs.sh
