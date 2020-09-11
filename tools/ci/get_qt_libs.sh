e78d6e88f8cb17629111ed1f4c7b38cfee2ff1ab
pestophagous <pestophagous@gmail.com>
2020-09-11 12:49:42 -0700

    get_qt_libs.sh: get icu-linux from download.qt.io so we stop versioning it
    this achieves two goals:
    - avoid 'libicui18n.so.56: cannot open shared object file: No such file or directory'
    - remove the multi-MB file libicudata.so.56 from the repo

--- tools/ci/get_qt_libs.sh
+++ tools/ci/get_qt_libs.sh
@@ -2,17 +2,23 @@
 
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Whoops! There is a SEPARATE script for Mac OSX. This one is linux-specific."
+  exit -1
+fi
+
 CUR_GIT_ROOT=$(git rev-parse --show-toplevel)
 
 DL_FOLDER=$CUR_GIT_ROOT/dl_third_party
+
+sudo pip3 install aqtinstall  # https://github.com/miurahr/aqtinstall
 
 if [ -d $DL_FOLDER/Qt_desktop/5.15.0/gcc_64/bin ]; then
     echo "no need to download qt5 for desktop"
 else
 
-  bash -x $CUR_GIT_ROOT/tools/ci/install-qt.sh \
-   --directory $DL_FOLDER/Qt_desktop \
-   --version 5.15.0 \
+  # https://github.com/miurahr/aqtinstall/issues/126 "Installing smaller subset of the libraries"
+  python3 -m aqt install --outputdir $DL_FOLDER/Qt_desktop 5.15.0 linux desktop --archives \
    qtbase \
    qtconnectivity \
    qtdeclarative \
@@ -20,26 +26,13 @@ else
    qtimageformats \
    qtquickcontrols \
    qtquickcontrols2 \
-   qtscript \
    qtsvg \
    qttools
 
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    bash -x $CUR_GIT_ROOT/tools/ci/install-qt.sh \
-     --directory $DL_FOLDER/Qt_desktop \
-     --version 5.15.0 \
-     --target ios \
-     qtbase \
-     qtconnectivity \
-     qtdeclarative \
-     qtgraphicaleffects \
-     qtimageformats \
-     qtquickcontrols \
-     qtquickcontrols2 \
-     qtsvg \
-     qttools
-
-    git checkout $CUR_GIT_ROOT/dl_third_party/Qt_desktop/5.15.0/ios/mkspecs/qconfig.pri
-  fi
-
 fi
+
+# A fix for: error while loading shared libraries: libicui18n.so.56: cannot open shared object file: No such file or directory
+pushd $DL_FOLDER/Qt_desktop/ >& /dev/null
+  wget https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5150/qt.qt5.5150.gcc_64/5.15.0-0-202005140804icu-linux-Rhel7.2-x64.7z
+  7z x 5.15.0-0-202005140804icu-linux-Rhel7.2-x64.7z
+popd
--- tools/ci/provision_mac.sh
+++ tools/ci/provision_mac.sh
@@ -24,4 +24,4 @@ pushd /usr/local/bin >& /dev/null
   sudo ln -sf clang-format clang-format-10
 popd >& /dev/null
 
-${CUR_GIT_ROOT}/tools/ci/get_qt_libs.sh
+${CUR_GIT_ROOT}/tools/ci/get_qt_libs_mac.sh
