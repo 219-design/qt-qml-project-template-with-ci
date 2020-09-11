ca55dcb3b47d52ff2c450755c52036d86c2d0ace
pestophagous <pestophagous@gmail.com>
2020-09-11 16:33:31 -0700

    use aqtinstall --archives (instead of wget) to get libicui18n.so

--- tools/ci/get_qt_libs.sh
+++ tools/ci/get_qt_libs.sh
@@ -19,6 +19,7 @@ else
 
   # https://github.com/miurahr/aqtinstall/issues/126 "Installing smaller subset of the libraries"
   python3 -m aqt install --outputdir $DL_FOLDER/Qt_desktop 5.15.0 linux desktop --archives \
+   icu \
    qtbase \
    qtconnectivity \
    qtdeclarative \
@@ -30,9 +31,3 @@ else
    qttools
 
 fi
-
-# A fix for: error while loading shared libraries: libicui18n.so.56: cannot open shared object file: No such file or directory
-pushd $DL_FOLDER/Qt_desktop/ >& /dev/null
-  wget https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5150/qt.qt5.5150.gcc_64/5.15.0-0-202005140804icu-linux-Rhel7.2-x64.7z
-  7z x 5.15.0-0-202005140804icu-linux-Rhel7.2-x64.7z
-popd
