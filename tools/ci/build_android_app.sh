1187685e5c9a715aa5fcb7da1627029221b1fd30
pestophagous <pestophagous@gmail.com>
2020-12-29 12:14:28 -0800

    enhance build_android_app so it can build 'flavors' in different dirs

--- tools/ci/build_android_app.sh
+++ tools/ci/build_android_app.sh
@@ -3,21 +3,28 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 CUR_GIT_ROOT=$(git rev-parse --show-toplevel)
-
 cd $CUR_GIT_ROOT
+
+echo "First argument to script is build output path"
+MYAPP_MOBILE_OUTPUT_PATH=$1
+shift 1
+
+echo "Next argument to script is qmake CONFIG token"
+MYAPP_MOBILE_CONFIG_TOKEN=$1
+shift 1
+
 
 tools/ci/version.sh
 
 source path_to_android_qmake.bash
 
-mkdir -p build/for_android
-cd build/for_android
-# When you need release: CONFIG+=release
-qmake CONFIG+=force_debug_info ../../ # debug info (symbols) are orthogonal to optimizations
+mkdir -p ${MYAPP_MOBILE_OUTPUT_PATH}/
+cd ${MYAPP_MOBILE_OUTPUT_PATH}/
+qmake ${MYAPP_MOBILE_CONFIG_TOKEN} ../../
 
 make
-make install INSTALL_ROOT=$CUR_GIT_ROOT/build/for_android/src/app/android-build
+make install INSTALL_ROOT=$CUR_GIT_ROOT/${MYAPP_MOBILE_OUTPUT_PATH}/src/app/android-build
 
-pushd $CUR_GIT_ROOT/build/for_android/src/app/ >& /dev/null
+pushd $CUR_GIT_ROOT/${MYAPP_MOBILE_OUTPUT_PATH}/src/app/ >& /dev/null
     make apk
 popd >& /dev/null
