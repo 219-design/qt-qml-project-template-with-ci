51bff1ce693cdd8012db35ae06528f44b8057d48
Chand John <ctj@alumni.stanford.edu>
2022-01-17 16:55:18 -0600

    Change TEST to TEST_F in navigation_test.cc
    Put a test fixture and TEST_F() tests based on that fixture instead
    of just a TEST() in navigation_test.cc.

--- src/lib_app/navigation_test.cc
+++ src/lib_app/navigation_test.cc
@@ -7,7 +7,49 @@
 //
 #include "gtest/gtest.h"
 
-TEST( NavigationTest, FirstDemo )
+#include <QString>
+
+namespace
 {
-    EXPECT_EQ( 2, 2 );
+class NavigationTest : public ::testing::Test
+{
+protected:
+    NavigationTest()
+        : m_name( "Fake Dialog" ), m_got_focus( false )
+    {
+    }
+
+    void GetFocus()
+    {
+        m_got_focus = true;
+    }
+    void LoseFocus()
+    {
+        m_got_focus = false;
+    }
+
+    const QString m_name;
+
+    bool m_got_focus;
+};
+
+TEST_F( NavigationTest, NameIsFakeDialog )
+{
+    EXPECT_EQ( m_name, QString( "Fake Dialog" ) );
 }
+
+TEST_F( NavigationTest, InitiallyLacksFocus )
+{
+    EXPECT_EQ( m_got_focus, false );
+}
+
+TEST_F( NavigationTest, GotFocus )
+{
+    GetFocus();
+    EXPECT_EQ( m_got_focus, true );
+
+    LoseFocus();
+    EXPECT_EQ( m_got_focus, false );
+}
+
+} // namespace
