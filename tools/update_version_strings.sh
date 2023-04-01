dcaa7a13157c99062f9454182392d6df0ad50a8b
pestophagous <pestophagous@gmail.com>
2023-04-01 13:48:25 -0700

    build script: short-circuit version script to save recompilation time

--- tools/update_version_strings.sh
+++ tools/update_version_strings.sh
@@ -18,6 +18,18 @@ BUILD_DATE=`date '+%Y-%m-%d'`
 
 LAST_KNOWN_HASH=`git rev-parse --verify --short=10 HEAD`
 
+if [ -f "${GENFILE}" ]; then
+  if [[ $(grep "${BUILD_DATE}" "${GENFILE}") ]]; then
+      echo "Correct preexisting date ${BUILD_DATE} found in ${GENFILE}"
+      if [[ $(grep "${LAST_KNOWN_HASH}" "${GENFILE}") ]]; then
+        echo "Correct preexisting git-hash ${LAST_KNOWN_HASH} found in ${GENFILE}"
+
+        # BAIL OUT EARLY!  DON'T EDIT THE HEADER (to avoid wasting recompilation time).
+        exit 0
+      fi
+  fi
+fi
+
 NEW_DATELINE="constexpr char BUILD_ON_DATE[] = \"${BUILD_DATE}\";"
 NEW_HASHLINE="constexpr char GIT_HASH_WHEN_BUILT[] = \"${LAST_KNOWN_HASH}\";"
 
